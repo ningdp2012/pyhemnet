8409e791d59789d7f5d00755aad7bb38d1289342
ningdp2012 <noreply@localhost>
2026-04-05 19:55:16 +0200

    feat: migrate to curl_cffi, bump to v1.0.0

--- pyhemnet/__init__.py
+++ pyhemnet/__init__.py
@@ -1,14 +1,13 @@
 """Swedish Real Estate Scraper - Scrape data from Hemnet.se"""
 
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 __author__ = "ningdp2012"
 
 # Import scraper classes
-from .hemnet import HemnetScraper
-from .qasa import QasaScraper
-
 # Import enums
 from .constants import HemnetItemType
+from .hemnet import HemnetScraper
+from .qasa import QasaScraper
 
 __all__ = [
     "HemnetScraper",
